1778880608e8839b5fce5d572cab3b147634d95c
Osama-Kyle <kt.osama09@gmail.com>
2013-08-28 21:49:32 +0200

    Added neccessary fields for game and changed constructor

--- src/Game.java
+++ src/Game.java
@@ -1,41 +1,41 @@
-
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Hashtable;
 
 public class Game extends Thread {
 
-    private String name;
-    private Server server;
-    //private int players;
-    public int numberOfGames;
+    public String name;
+    public Server server;
+    //Game number seems redundant?? But i'll leave it there for now, we might need it
+    public int gameNumber;
+    public int playerCount;
+    public int round;
+    public String trumpCard;
+    //Not sure if we need but we can always remove later
+    public String nextPlayerToPlay;
+    
+    //Hardcoded to 7 (max players) we can change this but hardcoding it to 7 wont really
+    //lead to any inefficiency
+    public int [] playerBets = new int [7];
+    public int [] playerScores = new int [7];
+    //Also hardcoded to 10 (max cards)
+    public String [][] playerCards = new String [7][10];
    
     
-    private HashSet<HandleClient> clientsList = new HashSet<HandleClient>();
+    public Hashtable<String, Integer> playerList = new Hashtable<String, Integer>();
 
-    
-    public Game(String name) {
-        this.name = name;
-    }
-
-    public Game(String gameName, HandleClient creator, Server server) {
-        name = gameName;
+    //Dont think we need more than one constructor?? Because theres only one way a game can be started
+    public Game(String gameName, String creator, Server server, int gameNumber) {
+        this.name = gameName;
         this.server = server;
-        //assuming clientsList will always be empty at first:
-        clientsList.add(creator);
+        this.playerList.put(creator, 1);
+        this.playerCount = 1;
+        this.round = 1;
+        this.gameNumber = gameNumber;
     }
-    
-    
-    
-   public Game(String gameName, Server server) {
-        name = gameName;
-        this.server = server;
-        //assuming clientsList will always be empty at first:
-    }
+   
 
     // deals all cards to users
     void deal(){
         //iterate through all users and add card into hands
     }
 }
-
 
