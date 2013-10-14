af3237eadc6e631d0ac5dc3980c436bba2c82ad0
Osama-Kyle <kt.osama09@gmail.com>
2013-10-14 16:53:51 +0200

    Update Server.java
    New server, started fresh

--- src/Server.java
+++ src/Server.java
@@ -1,231 +1,250 @@
-// 16561295
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.UIManager.LookAndFeelInfo;
-import javax.swing.*;
+import java.util.*;
 
-/**
- * 
- * @author //Ask
- * 
- */
-@SuppressWarnings("serial")
-public class Server extends JDialog implements ActionListener {
 
-    
-	private ServerSocket server;
-	private Socket socket;
-	private int port = 9119;
-	private boolean islistening;
+public class Server {
 
-	HashSet<HandleClient> clientsList = new HashSet<HandleClient>();
-	Hashtable<String, Game> gamesList = new Hashtable<String, Game>();
-	HandleClient client;
+	private ServerSocket ss;
 
-	// Gui
-	private JFrame frameMain;
-	private JTextArea textAreaServer;
-	private JTextArea textAreaClient;
-	private JTextArea textAreaGame;
-	private JPanel panelClient;
-	private JPanel panelServer;
-	private JPanel panelGame;
-	private JTextField textField;
-	private JButton button;
-	public int EddieTest = 1;
+	private Hashtable outputStreams = new Hashtable(); //socket, outputstream
+	Hashtable<String, Game> gamesList = new Hashtable<String, Game>(); //Gamename, game
+	private Hashtable <ObjectOutputStream,String> clientList = new Hashtable <ObjectOutputStream,String>();     //outputstream, username
 	
+	private final int MAX_PLAYERS = 7;
 
-	
-        private final int MAX_PLAYERS = 7;
+	public Server (int port) throws IOException{
 
-        public static void main(String[] args) {
-		try {
-			new Server().process();
-		} catch (Exception e) {
-			e.printStackTrace();
+		listen(port);
+	}
+
+	public static void main (String [] args) throws Exception{
+
+		int port = 3000;
+
+		Server server = new Server(port);
+
+	}
+
+	private void listen(int port) throws IOException{
+
+		ServerSocket ss = new ServerSocket(port);
+
+		System.out.println("Server started. Listening on " + ss);
+
+		// Keep listening on port and create a new thread for each socket accepted
+		while (true) {
+
+			Socket s = ss.accept();
+			
+			System.out.println("New connection from socket: "+ s);
+			
+			ObjectOutputStream output = new ObjectOutputStream(s.getOutputStream());
+			output.writeObject("RD;");
+
+			outputStreams.put(s, output);
+			
+			new HandleClient(this, s);
+		}
+
+
+
+	}
+
+	Enumeration getOutputStreams() {
+		return outputStreams.elements();
+	}
+
+	// Removes a connection from the hashtable
+	public void removeConnection(Socket s) {
+		synchronized (outputStreams) {
+		
+			System.out.println("Removing connection to " + s);
+		// Remove it from the hashtable
+			outputStreams.remove(s);
+			try {
+				s.close();
+			} catch (IOException ie) {
+				System.out.println("Error closing " + s);
+				ie.printStackTrace();
+			}
 		}
 	}
-	/**
-	 * Method : buildGui Builds the interface of server, clients and games. This
-	 * is a graphical representation of activity on server, clients and the
-	 * different games.
-	 */
-	public void buildGui() {
-		try {
-			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				if ("Nimbus".equals(info.getName())) {
-					UIManager.setLookAndFeel(info.getClassName());
+
+	//Sends a message to all clients connected to server
+	public void sendToAll(String message, Socket s) {
+
+		ObjectOutputStream sender = (ObjectOutputStream) outputStreams.get(s);
+
+		synchronized (outputStreams) {
+
+			for (Enumeration e = getOutputStreams(); e.hasMoreElements(); ) {
+
+				ObjectOutputStream output = (ObjectOutputStream)e.nextElement();
+				if (output != sender) {
+
+					try {
+						output.writeObject(message);
+						output.flush();
+					} catch (Exception e2) { 
+						System.out.println("IO exception in sendtoAll " + e2); 
+					}
+				}
+			}
+		}
+	}
+
+	//Sends a message to a specific client
+	public void sendToOne(String message, String username) {
+
+		//totest
+		ObjectOutputStream output = null;
+
+		synchronized (clientList) {
+			for (Enumeration e = clientList.keys(); e.hasMoreElements(); ) {
+				ObjectOutputStream o = (ObjectOutputStream) e.nextElement();
+				String user = (String) clientList.get(o);
+				if (user.equals(username)) {
+					output = o;
 					break;
 				}
 			}
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
 		}
-
-		JTabbedPane tab = new JTabbedPane();
-		frameMain = new JFrame();
-		frameMain.setTitle("Server Information");
-		frameMain.setSize(400, 400);
-		frameMain.setLocation(300, 300);
-
-		panelClient = new JPanel();
-		panelClient.setSize(frameMain.getWidth(), frameMain.getHeight());
-		panelClient.setLayout(null);
-
-		panelServer = new JPanel();
-		panelServer.setSize(frameMain.getWidth(), frameMain.getHeight());
-		panelServer.setLayout(null);
-
-		panelGame = new JPanel();
-		panelGame.setSize(frameMain.getWidth(), frameMain.getHeight());
-		panelGame.setLayout(null);
-
-		// TextArea for server
-		textAreaServer = new JTextArea();
-		textAreaServer.setEditable(false);
-		textAreaServer.setSize(frameMain.getWidth(), frameMain.getHeight());
-
-		// TextArea for clients
-		textAreaClient = new JTextArea();
-		textAreaClient.setEditable(false);
-		textAreaClient.setSize(frameMain.getWidth(), frameMain.getHeight());
-
-		// TextArea for games
-		textAreaGame = new JTextArea();
-		textAreaGame.setEditable(false);
-		textAreaGame.setSize(frameMain.getWidth(), frameMain.getHeight());
-
-		// Scrolepane for server
-		JScrollPane spMain = new JScrollPane(textAreaServer,
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		spMain.setSize(frameMain.getWidth(), frameMain.getHeight());
-		spMain.setLocation(0, 0);
-		panelServer.add(spMain);
-
-		// Scrolepane for clients
-		JScrollPane spClient = new JScrollPane(textAreaClient,
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		spClient.setSize(frameMain.getWidth(), frameMain.getHeight());
-		spClient.setLocation(0, 0);
-		panelClient.add(spClient);
-
-		// Scrolepane for Game
-		JScrollPane spGame = new JScrollPane(textAreaGame,
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		spGame.setSize(frameMain.getWidth(), frameMain.getHeight());
-		spGame.setLocation(0, 0);
-		panelGame.add(spGame);
-
-		tab.add("Server", panelServer);
-		tab.add("Clients", panelClient);
-		tab.add("Games", panelGame);
-
-		if (EddieTest == 1) {
-			textField = new JTextField();
-			textField.setBounds(200, 200, 100, 70);
-			button = new JButton("TEST");
-			button.addActionListener(this);
-			frameMain.add(textField, BorderLayout.SOUTH);
-			frameMain.add(button, BorderLayout.WEST);
-		}
-
-		frameMain.add(tab, BorderLayout.CENTER);
-		frameMain.setVisible(true);
-		frameMain.setResizable(false);
-		frameMain.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-		frameMain.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				quit();
-			}
-		});
-	}
-
-	public void process() {
-		// Build the GUI
-		buildGui();
-
-		// Open server port for communication
 		try {
-			islistening = true;
-			server = new ServerSocket(getPort());
-			textAreaServer.append("Server Successfully Started - port# : "
-					+ getPort() + "\n");
-			textAreaClient.append("Clients Connected to server:\n");
-			textAreaGame.append("Active games  : # Clients:\n");
-
-			// Run - accept the opening of server port
-			run();
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			islistening = false;
-			Logger.getLogger(Server.class.getName())
-					.log(Level.SEVERE, null, ex);
-
-			// Print Error to server text Area
-			textAreaServer
-					.append("Server not Started close all Connections and try again \n");
+			output.writeObject(message);
+			output.flush();
+		} catch (Exception e) { 
+			System.out.println("Exception in sendtoOne " + e); 
 		}
 	}
 
-	/**
-	 * Method : Run.
-         * This method accepts the connections and handles the clients
-	 */
-	public void run() {
-		while (isListening()) {
-			try {
-				socket = getServer().accept();
+	// Adds a username to the hashtable
+	public void addUsername(String username, Socket s) {
 
-				// Send msg to client that server is ready
-				PrintWriter output = new PrintWriter(getSocket().getOutputStream(),
-						true);
-				output.println("RD");
+		synchronized (outputStreams) {
+		
+			ObjectOutputStream output  = (ObjectOutputStream)outputStreams.get(s);	
+	
+			synchronized (clientList) {
 
-				// Handle clients in game
-				client = new HandleClient(getSocket(), this);
+				clientList.put(output, username);
 
-				if (clientsList.contains(client) == false) {
-					clientsList.add(client);
-					textAreaClient.append(client.getName() + "\n");
-					textAreaServer.append("new client : " + client.getName()
-							+ " : " + getTimeAndDate() + "\n");
-				} else {
-					JOptionPane.showMessageDialog(null,
-							"Server : Client NOT!! Unique\n");
-					client.closeConnection();
-				}
-			} catch (IOException ex) {
-                            textAreaClient.append("Error handling a socket connection request\n");
 			}
 		}
+	
 	}
 
+	// Removes a username from the hashtable
+	public void removeUsername(Socket s) {
+
+		synchronized (outputStreams) {
+		
+			ObjectOutputStream output  = (ObjectOutputStream)outputStreams.get(s);
+		
+			synchronized (clientList) {
+
+				clientList.remove(output);
+
+			}
+		}
+	
+	}
+
+	// Gets a username from a socket
+	public String getUsername(Socket s) {
+
+		ObjectOutputStream output  = (ObjectOutputStream)outputStreams.get(s);
+
+		return (String) clientList.get(output);
+
+	}
+
+	//Sends a list of all clientList connected to server to all clients connected to server
+	public void sendclientList() {
+
+		Collection Users = clientList.values();
+		String usernamelist = ".COMMAND_WHOSINTHEROOOM";
+		Iterator itr = Users.iterator();
+		while (itr.hasNext()) {
+			usernamelist += " " + itr.next();
+		}
+
+		for (Enumeration e = getOutputStreams(); e.hasMoreElements(); ) {
+
+			ObjectOutputStream output = (ObjectOutputStream)e.nextElement();
+			
+
+			try {
+				output.writeObject(usernamelist);
+				output.flush();
+				
+			} catch (Exception e2) { 
+				System.out.println("IO exception in send clientList " + e2); 
+			}
+			
+		}
+	}
+	
+	
+	
+// END OF OLD METHODS: //
+	
+	
+	
+	
+	
+	// ----------- ///
+	
+	
+	
+	
+	
+	
+// START OF NEW METHODS: //
+	
+	public void login(String username, String password, Socket socket) {
+		
+		Collection Users = clientList.values();
+		ObjectOutputStream o = null;
+		
+		try {
+			o = (ObjectOutputStream) socket.getOutputStream();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Exception in login " + e1);
+		}
+		
+		Iterator itr = Users.iterator();
+		
+		while (itr.hasNext()) {
+			if (username.equals(itr.next())) {
+				try {
+					o.writeObject("ER100;");
+					o.flush();
+					return;
+				} catch (IOException e) {
+					
+					System.out.println("Exception in login " + e);
+				}
+			}
+		}
+		
+		try {
+			
+			clientList.put(o, username);
+			
+			o.writeObject("LK;");
+			o.flush();
+			return;
+		} catch (IOException e) {
+			
+			System.out.println("Exception in login " + e);
+		}
+		
+	}
+	
 	public String getTimeAndDate() {
 		DateFormat dateFormat = new SimpleDateFormat("dd MMM hh:mm");
 		Calendar cal = Calendar.getInstance();
@@ -324,28 +343,6 @@ public class Server extends JDialog implements ActionListener {
 	public boolean joinGame(String gameName, String playerName) {
 		return (gamesList.get(gameName).addPlayer(playerName));
 	}
-
-	public void quit() {
-		try {
-			getServer().close();
-			islistening = false;
-			frameMain.dispose();
-			System.exit(0);
-		} catch (IOException e1) {
-                    //If there is something wrong, quit anyway.
-			System.exit(0);
-		}
-	}
-
-	public void actionPerformed(ActionEvent evt) {
-		if (evt.getSource() == button) {
-			String text = textField.getText();
-			text = text + "sad";
-			System.out.println(text);
-			textField.setText(" ");
-		}
-	}
-
 	
 	//Get games list as string array for client to choose a game
     String[] getGamesList() {
@@ -360,89 +357,8 @@ public class Server extends JDialog implements ActionListener {
         }
         return stringGamesList;
     }
-    
-    //Same as above function but takes in a prefix parameter
-    String[] getGamesList(String prefix) {
-        String[] stringGamesList;
-        int length = gamesList.size();
-        int counter = 0;
-        stringGamesList = new String[length];
-        Iterator<String> itr = gamesList.keySet().iterator();
-        String tmpGameName;
-        while (itr.hasNext()) {
-            tmpGameName = itr.next();
-            if (tmpGameName.startsWith(prefix)) {
-            	stringGamesList[counter] = tmpGameName;
-            	counter++;
-            }
-            
-        }
-        if (counter == 0) {
-        	stringGamesList[counter] = "";
-        }
-        return stringGamesList;
-    }
 
-    /**
-     * @return the server
-     */
-    public ServerSocket getServer() {
-        return server;
-    }
 
-    /**
-     * @return the socket
-     */
-    public Socket getSocket() {
-        return socket;
-    }
-
-    /**
-     * @return the port
-     */
-    public int getPort() {
-        return port;
-    }
-
-    /**
-     * @return the islistening
-     */
-    public boolean isListening() {
-        return islistening;
-    }
-    
-    /**
-     * @return the Number of games currently in gamesList HashTable
-     */
-    public int getGamesCount() {
-        return gamesList.size();
-    }
-
-    Game getGame(String gameName) {
-        return gamesList.get(gameName);
-    }
-    
-	public boolean logoffClient(String userName) {
-		HandleClient client = getHandle(userName);
-		if(clientsList.contains(client)){
-			clientsList.remove(client);
-			return true;
-		}
-		return false;
-	}
-	
-	public HandleClient getHandle(String playerName){
-		Iterator<HandleClient> itr = clientsList.iterator();
-		HandleClient tempClient, client = null;
-		
-		while(itr.hasNext()){
-			tempClient = itr.next();
-			if(tempClient.getName().equals(playerName)){
-				//there is such a user that can be kicked.
-				client = tempClient;
-				break;
-			}
-		}
-		return client;
-	}
 }
+
+
