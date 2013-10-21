7c25a460e2a34db94f9ad464f27a7c4ea7be1165
Eaton Emmerich <e.e.metabee@gmail.com>
2013-10-21 23:34:42 +0200

    Test updates 4 server.

--- src/TestInvalidInput.java
+++ src/TestInvalidInput.java
@@ -40,6 +40,13 @@ public class TestInvalidInput{
 	private String input2;
 	private String input3;
 	private String input4;
+	private Socket client3;
+	private Socket client2;
+	private ObjectOutputStream objectOutput2;
+	private ObjectOutputStream objectOutput3;
+	private ObjectInputStream objectInput2;
+	private ObjectInputStream objectInput3;
+	private String input5;
     
     @Parameters
    public static Collection messageInputs() throws FileNotFoundException, IOException {
@@ -49,41 +56,48 @@ public class TestInvalidInput{
         String[] c = read.readLine().split(",");
         
         String[] d = read.readLine().split(",");
-        String [][] e;
-        int maxlength = Math.max(a.length, Math.max(b.length, Math.max(c.length,d.length)));
-        e = new String [maxlength][4];
+        String[] e = read.readLine().split(",");
+        String [][] end;
+        int maxlength = Math.max(a.length, Math.max(b.length, Math.max(c.length,Math.max(d.length,e.length))));
+        end = new String [maxlength][5];
         
         for(int i = 0; i < a.length; i++){
         	if(i<a.length){
-        		e[i][0] = a[i];
+        		end[i][0] = a[i];
         	}else{
-        		e[i][0] = "GNGamename;";
+        		end[i][0] = "GNGamename;";
         	}
         	if(i<b.length){
-	        	e[i][1] = b[i];
+	        	end[i][1] = b[i];
         	}else{
-        		e[i][1] = "GNGamename;";
+        		end[i][1] = "GNGamename;";
         	}
         	if(c.length>i){
-	        	e[i][2] = c[i];
+	        	end[i][2] = c[i];
         	}else{
-        		e[i][2] = "LIusername:password;";
+        		end[i][2] = "LIusername:password;";
         	}
         	if(d.length>i){
-	        	e[i][3] = d[i];
+	        	end[i][3] = d[i];
         	}else{
-        		e[i][3] = "&*^%$;";
+        		end[i][3] = "&*^%$;";
+        	}
+        	if(e.length>i){
+        		end[i][4] = e[i];
+        	}else{
+        		end[i][4] = "LIusername:password";
         	}
         }
         
-        return Arrays.asList(e);
+        return Arrays.asList(end);
    }
 
-    public TestInvalidInput(String input,String input2,String input3, String input4) {
+    public TestInvalidInput(String input,String input2,String input3, String input4,String input5) {
         this.input = input;
         this.input2 = input2;
         this.input3 = input3;
         this.input4 = input4;
+        this.input5 = input5;
     }
     
 
@@ -94,16 +108,22 @@ public class TestInvalidInput{
     	serverThread.start();
     	Thread.sleep(100);
         client = new Socket("localhost", 3000);
+        client2 = new Socket("localhost", 3000);
+        client3 = new Socket("localhost", 3000);
         objectInput = new ObjectInputStream(client.getInputStream());
-        objectOutput = new ObjectOutputStream(client.getOutputStream());   
+        objectOutput = new ObjectOutputStream(client.getOutputStream());
+        objectInput2 = new ObjectInputStream(client2.getInputStream());
+        objectOutput2 = new ObjectOutputStream(client2.getOutputStream());
+        objectInput3 = new ObjectInputStream(client3.getInputStream());
+        objectOutput3 = new ObjectOutputStream(client3.getOutputStream());
         String error = (String) objectInput.readObject();
-        	
-        
     }
 
     @After
     public void tearDown() throws Exception {
         client.close();
+        client2.close();
+        client3.close();
         server.closeServer();
         server = null;
     }
@@ -171,6 +191,62 @@ public class TestInvalidInput{
         assertTrue("recieved \'" + error + "\' instead of \'ER900;\' @ "+ input4, error.equals("ER900;"));
     }
     
+    @Test
+    public void testInvalidInputsAfterGameCreationNotAMessage() throws Exception{
+    	objectOutput.writeObject("LIusername:Password;");
+    	objectOutput.flush();
+    	String rd = (String)objectInput.readObject();
+    	
+    	objectOutput.writeObject("GSgamename;");
+    	objectOutput.flush();
+    	rd = (String)objectInput.readObject();
+    	
+        objectOutput.writeObject(input4);
+        objectOutput.flush();
+        String error = (String) objectInput.readObject();
+        assertTrue("recieved \'" + error + "\' instead of \'ER900;\' @ "+ input4, error.equals("ER900;"));
+    }
     
+    @Test
+    public void testInvalidInputsGameStartPlayer1() throws Exception{
+    	setupGameStart();
+    	
+        objectOutput.writeObject(input5);
+        objectOutput.flush();
+        
+        String error = (String) objectInput.readObject();
+        assertTrue("recieved \'" + error + "\' instead of \'ER901;\' @ "+ input5, error.equals("ER901;"));
+    }
+
+	private void setupGameStart() throws Exception {
+		objectOutput.writeObject("LIusername:Password;");
+    	objectOutput.flush();
+    	String rd = (String)objectInput.readObject();
+    	
+    	objectOutput.writeObject("GSgamename;");
+    	objectOutput.flush();
+    	rd = (String)objectInput.readObject();
+    	
+    	objectOutput2.writeObject("LIusername2:Password;");
+    	objectOutput2.flush();
+    	rd = (String)objectInput2.readObject();
+    	objectOutput3.writeObject("LIusername3:Password;");
+    	objectOutput3.flush();
+    	rd = (String)objectInput3.readObject();
+    	
+    	objectOutput2.writeObject("GJgamename;");
+    	objectOutput2.flush();
+    	rd = (String)objectInput2.readObject();
+    	objectOutput3.writeObject("GJgamename;");
+    	objectOutput3.flush();
+    	rd = (String)objectInput3.readObject();
+    	Thread.sleep(100);
+    	objectOutput.writeObject("GPgamename;");
+    	objectOutput.flush();
+    	rd = (String)objectInput.readObject();
+    	
+    	
+		
+	}
     
 }
