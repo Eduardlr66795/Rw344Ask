3192fdc3e78c51f2ba0770c2b38d8df811c0bfa1
Osama-Kyle <kt.osama09@gmail.com>
2013-10-22 01:59:51 +0200

    Update HandleClient.java
    Fully tested HandleClient

--- src/HandleClient.java
+++ src/HandleClient.java
@@ -20,82 +20,177 @@ public class HandleClient extends Thread {
 
 			while (true) {
 				String message = (String) input.readObject();
+				
+				if (message.length() < 3) {
+					server.badMessageFormat(socket);
+					continue;
+				}
+				
 				String command = message.substring(0, 2);
 				String[] arguments = message.substring(2).replace(";", "")
 						.split(":");
 
 				if (command.equals("LI")) {
-					server.login(arguments[0], arguments[1], socket);
+					if (arguments.length != 2) {
+						server.badMessageFormat(socket);
+					} else {
+						server.login(arguments[0], arguments[1], socket);
+					}
 
 				} else if (command.equals("LO")) {
-					server.logoff(socket);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.logoff(socket);
+					}
 
 				} else if (command.equals("GS")) {
-					server.createGame(arguments[0], socket);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.createGame(arguments[0], socket);
+					}
 
 				} else if (command.equals("GN")) {
-					server.addPlayerToGame(arguments[0], socket);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.addPlayerToGame(arguments[0], socket);
+					}
 
 				} else if (command.equals("GF")) {
-					server.gameIsFull(arguments[0], socket);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.gameIsFull(arguments[0], socket);
+					}
 
 				} else if (command.equals("GO")) {
-					server.kickPlayerFromGame(arguments[0], arguments[1],
-							socket);
+					if (arguments.length != 2) {
+						server.badMessageFormat(socket);
+					} else {
+						server.kickPlayerFromGame(arguments[0], arguments[1],socket);
+					}
 
 				} else if (command.equals("GL")) {
-					server.getGamesList(socket, "");
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.getGamesList(socket, "");
+					}
 
 				} else if (command.equals("GG")) {
-					server.getGamesList(socket, arguments[0]);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.getGamesList(socket, arguments[0]);
+					}
+					
 
 				} else if (command.equals("GJ")) {
-					server.joinGame(socket, arguments[0]);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.joinGame(socket, arguments[0]);
+					}
 					
 				} else if (command.equals("GW")) {
-					server.waitForGame(socket, arguments[0]);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.waitForGame(socket, arguments[0]);
+					}
 					
 				} else if (command.equals("GA")) {
-					server.playersInGame(socket, arguments[0]);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.playersInGame(socket, arguments[0]);
+					}
 					
 				} else if (command.equals("QT")) {
-					server.quitGame(socket, arguments[0]);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.quitGame(socket, arguments[0]);
+					}
 				
 				} else if (command.equals("LC")) {
                     server.sendclientList();
                     
 				} else if (command.equals("HN")) {
-					server.handRequest(socket, arguments[0]);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.handRequest(socket, arguments[0]);
+					}
 				
 				} else if (command.equals("HB")) {
-					server.bidRequest(socket, arguments[0]);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.bidRequest(socket, arguments[0]);
+					}
 					
 				} else if (command.equals("HD")) {
-					server.bidPlay(socket, arguments[0], arguments[1]);
+					if (arguments.length != 2) {
+						server.badMessageFormat(socket);
+					} else {
+						server.bidPlay(socket, arguments[0], arguments[1]);
+					}
 					
 				} else if (command.equals("HP")) {
-					server.cardRequest(socket, arguments[0]);
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.cardRequest(socket, arguments[0]);
+					}
 					
 				} else if (command.equals("HR")) {
-					server.cardPlay(socket, arguments[0], arguments[1]);
+					if (arguments.length != 2) {
+						server.badMessageFormat(socket);
+					} else {
+						server.cardPlay(socket, arguments[0], arguments[1]);
+					}
 				
 				} else if (command.equals("HA")) {
-					server.anotherHand(socket, arguments[0]);	
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.anotherHand(socket, arguments[0]);
+					}
 					
 				} else if (command.equals("HS")) {
-					server.scoreRequest(socket, arguments[0]);	
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.scoreRequest(socket, arguments[0]);
+					}
 					
 				} else if (command.equals("CP")) {
+					// CP arguments are checked in server method
 					server.chatToPlayers(socket, arguments);
 					
 				} else if (command.equals("CA")) {
-					server.chatToGame(socket, arguments[0], arguments[1]);
+					if (arguments.length != 2) {
+						server.badMessageFormat(socket);
+					} else {
+						server.chatToGame(socket, arguments[0], arguments[1]);
+					}
 					
 				} else if (command.equals("CC")) {
-					server.collectMessages(socket);	
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.collectMessages(socket);
+					}
 					
 				} else if (command.equals("MC")) {
-					server.modeConfusion(socket);		
+					if (arguments.length != 1) {
+						server.badMessageFormat(socket);
+					} else {
+						server.modeConfusion(socket);
+					}
 
 				} else {
 					server.badMessageFormat(socket);
